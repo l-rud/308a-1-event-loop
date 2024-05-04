3e2b53f40e7854f7b3b20966351b29851a659341
Larysa Rud <larpavrud@gmail.com>
2024-05-04 17:53:44 -0400

    Write a recursive function that completely flattens an array of nested arrays

--- script.js
+++ script.js
@@ -14,3 +14,33 @@ try {
   console.log(err);
   console.log("The maximum size of the JavaScript call stack is a " + counter);
 }
+
+// Part 2: Trampolines
+//Write a recursive function that completely flattens an array of nested arrays, 
+//regardless of how deeply nested the arrays are.
+
+// original nested array:
+const arr = [[1, [2, 3, [[4, 5], 6]], 7], 8, 9];
+
+// array that will store flattened values:
+const result = [];
+
+
+//https://dev.to/babak/understanding-recursion-using-iteration-or-vice-versa-3l5o
+//("Bonus: Other Ways To Solve Recursively")
+function flattenArrNoEncapsulationNotPure( arr, result = [] ) {
+    for( let item of arr ) {
+      if( Array.isArray(item) ) {
+        flattenArrNoEncapsulationNotPure( item, result ) 
+      } else {
+        result.push( item )
+      }
+    }
+    return result
+  }
+
+// flatten the array recursively
+flattenArrNoEncapsulationNotPure(arr, result);
+
+// output the flattened array
+console.log(result);
